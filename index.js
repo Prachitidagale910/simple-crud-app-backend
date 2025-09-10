0cc5cd9055b41e396ac5a54686b4e158ba79c5f6
Ranu_RD93 <148703739+Prachitidagale910@users.noreply.github.com>
2025-09-10 07:15:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,29 +18,12 @@ app.get('/', (req,res)=>{
     res.send("Hello from Node");
 });
 
+// routes for ('/app/products/)
 app.use("/api/products", routeProducts);
-// app.unsubscribe("/api/products", prodctRoute);
 
 
-// // reading the data
-// app.get("/api/products/", );
 
-// // view by id 
-// // reading the data
-// // here modified path : products -> product
-// app.get("/api/product/:id",);
-
-// app.post("/api/products/",);
-
-// // update a product 
-// app.put("/api/product/:id", );
-
-// // delete a product 
-
-// app.delete("/api/product/:id", )
-
-
-mongoose.connect("mongodb+srv://prachitidagale_db_user:basic_crud_app@backenddb.q1z3rtg.mongodb.net/Node-API?retryWrites=true&w=majority&appName=backenddb").then(()=>{
+mongoose.connect("mongodb+srv://<username>:<password>backenddb.q1z3rtg.mongodb.net/Node-API?retryWrites=true&w=majority&appName=backenddb").then(()=>{
 
     console.log("connection succcesssful.....")
 }).catch(()=>{
