6aee7934bbe58f659160957fcf42b805c2d77f07
Prachitidagale910 <dagaleprachiti910@gmail.com>
2025-09-10 08:23:22 +0530

    Remove old MongoDB URI from index.js

--- index.js
+++ index.js
@@ -1,31 +1,44 @@
-const Product = require('./models/product.model.js');
+require("dotenv").config({ override: true }); // loads .env variables
 
-const express = require('express')
-const app = express()
-const mongoose = require('mongoose')
-const routeProducts = require("./routes/product.route.js")
+const Product = require("./models/product.model.js");
+
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const routeProducts = require("./routes/product.route.js");
 
 // middlewares
-app.use(express.json()); 
-app.use(express.urlencoded({extended:false}))
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-
-app.listen(3000, ()=>{
-    console.log("server is running on port 3000");
+app.listen(3000, () => {
+  console.log("server is running on port 3000");
 });
 
-app.get('/', (req,res)=>{
-    res.send("Hello from Node");
+app.get("/", (req, res) => {
+  res.send("Hello from Node");
 });
 
 // routes for ('/app/products/)
 app.use("/api/products", routeProducts);
 
+// // view by id
+// // reading the data
+// // here modified path : products -> product
+// app.get("/api/product/:id",);
 
+// app.post("/api/products/",);
 
-mongoose.connect("mongodb+srv://<username>:<password>backenddb.q1z3rtg.mongodb.net/Node-API?retryWrites=true&w=majority&appName=backenddb").then(()=>{
+// // update a product
+// app.put("/api/product/:id", );
 
-    console.log("connection succcesssful.....")
-}).catch(()=>{
-    console.log("error in the connection.")
-})
+// // delete a product
+// app.delete("/api/product/:id", );
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("DB Connected!"))
+  .catch((err) => console.log("DB Error:", err));
